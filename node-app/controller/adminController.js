ac5a7ec4b1364e50c68a3ea5488e8557576e2a05
florentin <florentin1.ganfon@epitech.eu>
2025-03-26 09:46:04 +0000

    ojodfe

--- node-app/controller/adminController.js
+++ node-app/controller/adminController.js
@@ -30,7 +30,7 @@ exports.getUser = async (req,res) => {
 }
 
 exports.getallUser = async (req,res) => {
-    const user = await User.find({name: {$ne: "admin"}})
+    const user = await User.find({username: {$ne: "admin"}})
     if (user) {
         res.send({message: user})
     } else {
